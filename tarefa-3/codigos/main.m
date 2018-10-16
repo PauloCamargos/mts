9a9f9c6f781a4aec1b6b7750d20e0c3b1bf12088
PauloCamargos <paulo.camargos@ufu.br>
2018-10-16 19:58:40 -0300

    tutorial canvas; adicao pasta projeto final

--- tarefa-3/codigos/main.m
+++ tarefa-3/codigos/main.m
@@ -8,46 +8,61 @@ clear, clc;
 %  TAREFAS A SEREM FEITAS
 % 1) Ler o arquivo de dados do paciente
 load ('..\dados\C0005201_Paciente.mat') % Mude aqui o local da sua variav.
+potencia_paciente = Potencia_Total_Sinal;
+clear Potencia_Total_Sinal;
+load ('..\dados\C0005201_RUIDO.mat') % Mude aqui o local da sua variav.
+ruido_paciente = Potencia_Total_Sinal;
+clear Potencia_Total_Sinal;
 
-% 1) Pegar a primeira epoca da variael xn
-epoca_um = epochsValues{1};
+vetor_validos = [4,5,6,8,9,10,11,12,13,14,15,16,18,19,20];
+potencia_valida = potencia_paciente(vetor_validos,:);
+ruido_valido = ruido_paciente(vetor_validos,:);
 
-% 1) Pegar a potencia da primeira epoca
-potencia_epoca_um = Potencia_Total_Sinal(:,1);
-
-% 1) Médias
-media_epoca_um = mean(epoca_um);
-media_potencia = mean(potencia_epoca_um);
-
-% 1) Desvio padrao
-dv_epoca_um = std(epoca_um);
-dv_potencia = std(potencia_epoca_um);
-
-% 1) Kurtosis
-k_epoca_um = kurtosis(potencia_epoca_um);
-k_potencia = kurtosis(epoca_um);
-
-% 1) Calculo de um terco das epocas
-terco = epoca_um(:,1:length(epoca_um)/3);
+percentual_ruido = (ruido_valido ./ potencia_valida) * 100;
 
 
-% 1) Exibicao do histograma do sinal xn
-subplot(1,3,1);
-histfit(xn(1,:));
-title('XN');
-ylabel("Nº Ocorrencias");
-xlabel("Tensão [uV]");
-
-% 1) Exibicao do histograma do sinal epoca_um
- subplot(1,3,2);
-histfit(epoca_um(1,:));
-title('Época 1');
-ylabel("Nº Ocorrencias");
-xlabel("Tensão [uV]");
-
-% 1) Exibicao do histograma do sinal terco
-subplot(1,3,3);
-histfit(terco(1,:));
-title('Terço da Época 1');
-ylabel("Nº Ocorrencias");
-xlabel("Tensão [uV]");
+% % 1) Pegar a primeira epoca da variael xn
+% epoca_um = epochsValues{1};
+% 
+% % 1) Pegar a potencia da primeira epoca
+% potencia_epoca_um = Potencia_Total_Sinal(:,1);
+% 
+% % 1) Médias
+% media_epoca_um = mean(epoca_um);
+% media_potencia = mean(potencia_epoca_um);
+% 
+% % 1) Desvio padrao
+% dv_epoca_um = std(epoca_um);
+% dv_potencia = std(potencia_epoca_um);
+% 
+% % 1) Kurtosis
+% k_epoca_um = kurtosis(potencia_epoca_um);
+% k_potencia = kurtosis(epoca_um);
+% 
+% % 1) Calculo de um terco das epocas
+% terco = epoca_um(:,1:length(epoca_um)/3);
+% 
+% 
+% % 1) Exibicao do histograma do sinal xn
+% subplot(1,3,1);
+% histfit(xn(1,:));
+% title('XN');
+% ylabel("Nº Ocorrencias");
+% xlabel("Tensão [uV]");
+% 
+% % 1) Exibicao do histograma do sinal epoca_um
+%  subplot(1,3,2);
+% histfit(epoca_um(1,:));
+% title('Época 1');
+% ylabel("Nº Ocorrencias");
+% xlabel("Tensão [uV]");
+% 
+% % 1) Exibicao do histograma do sinal terco
+% subplot(1,3,3);
+% histfit(terco(1,:));
+% title('Terço da Época 1');
+% ylabel("Nº Ocorrencias");
+% xlabel("Tensão [uV]");
+% 
+% % Pegar a potencia total e comparar o valor do ruído por eletrodo e dar o
+% % resultado em porcentagem.
---
+++ trabalho-final/canvas.md
@@ -0,0 +1,49 @@
+# CANVAS
+
+## CLIENTES
+Divisões dos clientes de acordo com suas necessidades, constumes ou outros hábitos em comum.
+Definição de segmento de clientes.
+
+## Proposta de valor
+
+Como a empresa cria determinado valor para um determinado segmento de cliente
+e se diferencia da conconrrência: o que o seu produto leva de vantagem.
+
+Ex.: Subway: lanches rápidos, saudáveis e personalizados.
+
+## Canais
+
+Formas como um empresa comunica e entrega sua proposta de valor para cada seg. de clientes.
+
+## Relacionamento com o cliente
+
+Forma como a empresa interage com um segmento de cliente, de forma que seu cliente volte sempre a comprar.
+
+## Fonte de receita
+
+Forma como uma empresa gera receita através de cada segmento de cliente.
+
+## Recursos-chave
+
+O que é necessário para a empresa sair do papel e virar um negócio real.
+Principais recursos necessários para que uma empresa faça seu modelo de negócios funcionar.
+
+## Atividades-chave
+
+Atividades essenciais para o que o modelo de negócios da empresa funcione corretamente.
+
+## Parceiros-chave
+
+Quem são as empresas, instituições ou pessoas que são importantes para o funcionamento 
+do modelo de negócios.
+
+- Cores (do postit) definem se depende ou não do cliente. Back e front stage.
+## Estrutura de custos
+
+Envolve os principais custos decorrente da operação do modelo de negócios.
+
+## Locais para fazer o canvas:
+
+[Startup Biz Model](startupbizmodel.com)
+[]()
+
